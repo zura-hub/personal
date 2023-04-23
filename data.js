c1955ce8485b03213f820ed7e707c088272fa0d1
Zviad <zvsajaia@protonmail.com>
2023-04-23 15:17:26 +0400

    next commit

--- data.js
+++ data.js
@@ -9,55 +9,60 @@ const textInput = document.querySelector('#text-area')
 
 // validation functions
 
- nameInput.addEventListener('input', function() {
+const chackedName = nameInput.addEventListener('input', function () {
     if (nameInput.value.trim() === '') {
-      nameError.textContent = 'Enter valid name';
-      return false;
+        nameError.textContent = 'Enter valid name';
+        return false;
     }
-  
+
     if (nameInput.value.trim().length <= 1) {
-      nameError.textContent = 'Name must contain at least two characters';
-      return false;
+        nameError.textContent = 'Name must contain at least two characters';
+        return false;
     }
-  
+
     nameError.textContent = '';
     return true;
-  });
+});
 
 
-  meilInput.addEventListener('input' , function(){
-    if (meilInput.value.trim() === ' '){
-        mailError.textContent = 
-        'Enter valid email'
+const chackedMail = meilInput.addEventListener('input', function () {
+    if (meilInput.value.trim() === ' ') {
+        mailError.textContent =
+            'Enter valid email'
         return false
     }
     if (!meilInput.value.trim().includes('@')
-        || !meilInput.value.trim().includes('.')        
-    )
-    {
+        || !meilInput.value.trim().includes('.')
+    ) {
         mailError.textContent =
-        'Email format must @example.com'
+            'Email format must @example.com'
         return false
     }
 
     mailError.textContent = ''
     return true
-  })
+})
 
 
-  textInput.addEventListener('input' , function () {
-    if (textInput.value.trim() === ''){
+const chackedText = textInput.addEventListener('input', function () {
+    if (textInput.value.trim() === '') {
         textError.textContent =
-        'Text area must field'
+            'Text area must field'
         return false
     }
-    if (textInput.value.trim().length <= 20){
-        textError.textContent = 
-        'Text mus contain at last 20 words'
+    if (textInput.value.trim().length <= 20) {
+        textError.textContent =
+            'Text mus contain at last 10 words'
+        return false
     }
-  })
+
+    textError.textContent = ''
+    return true
+})
 
 
-  function errorField() {
-    alert ('Please fill in the contact details')
-  }
+function errorField() {
+
+    alert('Fill in contact details')
+
+}
